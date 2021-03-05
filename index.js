7219626c08de2be874c2cd9062e8fad306d2ea0c
KVexcavator <str_token@mail.ru>
2021-03-05 11:28:06 +0300

    finish

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const WebSocket = require("ws")
 const http = require("http")
 const {v4: uuidv4} = require("uuid")
-const { sign } = require("crypto")
 
 const app = express()
 const port = process.env.PORT || 9000
